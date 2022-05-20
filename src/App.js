9ba264e2d7c490ecb36315d03012da14ea0912f9
le quoc anh <lequocanh662000@gmail.com>
2022-05-20 15:36:32 +0700

    add-redux-filter

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Provider } from 'react-redux';
 import Cart from './component/Cart';
 import Filter from './component/filter';
 import Products from './component/Products';
-import data from './data';
 import store from './store';
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
-      size: '',
-      sort: '',
-      ratingsort: '',
     };
     // this.filterProducts = this.filterProducts.bind(this);
   }
@@ -51,70 +46,6 @@ class App extends React.Component {
     this.setState({ cartItems });
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
-  //need to change method function
-  sortProducts = (event) => {
-    //imp
-    const sort = event.target.value;
-    console.log(event.target.value);
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === 'lowest'
-            ? a.price < b.price
-              ? 1
-              : -1
-            : sort === 'highest'
-            ? a.price > b.price
-              ? 1
-              : -1
-            : a._id > b._id
-            ? 1
-            : -1
-        ),
-    }));
-  };
-
-  ratingProducts = (event) => {
-    //imp
-    const ratingsort = event.target.value;
-    console.log(event.target.value); //for testing whenever ya change the state
-    this.setState((state) => ({
-      ratingsort: ratingsort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          ratingsort === 'lowest'
-            ? a.rating < b.rating
-              ? 1
-              : -1
-            : ratingsort === 'highest'
-            ? a.rating > b.rating
-              ? 1
-              : -1
-            : a._id > b._id
-            ? 1
-            : -1
-        ),
-    }));
-  };
-
-  filterProducts = (event) => {
-    //imp
-    console.log(event.target.value);
-    if (event.target.value === '') {
-      this.setState({ size: event.target.value, products: data.products });
-    } else {
-      //have errors
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  };
 
   render() {
     return (
@@ -126,19 +57,8 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter
-                  count={this.state.products.length}
-                  sort={this.state.sort}
-                  ratingsort={this.state.ratingsort}
-                  size={this.state.size}
-                  sortProducts={this.sortProducts}
-                  ratingProducts={this.ratingProducts}
-                  filterProducts={this.filterProducts}
-                ></Filter>
-                <Products
-                  products={this.state.products}
-                  addToCart={this.addToCart}
-                ></Products>
+                <Filter></Filter>
+                <Products addToCart={this.addToCart}></Products>
               </div>
               <div className="sidebar">
                 <Cart
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,9 +1,36 @@
-import { FETCH_PRODUCTS } from '../type';
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+  ORDER_PRODUCTS_BY_RATING,
+} from '../type';
 
 export const productsReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_PRODUCTS:
-      return { items: action.payload };
+      return { items: action.payload, filteredItems: action.payload };
+
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        size: action.payload.size,
+        filteredItems: action.payload.items,
+      };
+
+    case ORDER_PRODUCTS_BY_PRICE:
+      return {
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
+
+    case ORDER_PRODUCTS_BY_RATING:
+      return {
+        ...state,
+        ratingsort: action.payload.ratingsort,
+        filteredItems: action.payload.items,
+      };
+
     default:
       return state;
   }
